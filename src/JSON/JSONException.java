b87d251e535e3eb2e4f9a577c37c2e3f33032481
stcksmsh <vukicevickosta@gmail.com>
2023-06-27 04:17:12 +0200

    Possibly all done needs testing and bug fixing

--- src/JSON/JSONException.java
+++ src/JSON/JSONException.java
@@ -4,4 +4,9 @@ public class JSONException extends RuntimeException {
     public JSONException(String errorMSG) {
         super(errorMSG);
     }
+
+    public JSONException(int index, String string) {
+        super("Error: unexpected character '" + string.charAt(index) + "' encountered at index "
+                + Integer.toString(index) + " while parsing string:" + string);
+    }
 }
--- src/JSON/JSONObject.java
+++ src/JSON/JSONObject.java
@@ -1,12 +1,10 @@
 package JSON;
 
+import java.security.CryptoPrimitive;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
-import javax.print.DocFlavor.STRING;
-
-import JSON.JSONException;
+import java.util.Stack;
 
 public class JSONObject {
 
@@ -19,7 +17,7 @@ public class JSONObject {
         super();
         keys = null;
         values = null;
-        type = 'N'; /// NULL object
+        type = 'U'; /// Undefined object
         value = null;
     }
 
@@ -48,6 +46,11 @@ public class JSONObject {
         type = 'I';
     }
 
+    public void setNull(){
+        value = null;
+        type = 'N';
+    }
+
     public void setValue(double d) {
         value = Double.valueOf(d);
         type = 'D';
@@ -63,10 +66,196 @@ public class JSONObject {
         type = 'B';
     }
 
-    public static JSONObject parseString(String string) {
-        JSONObject obj = new JSONObject();
+    public char getType() {
+        return type;
+    }
 
-        return obj;
+    public boolean isEmpty(){
+        if(type == 'A' || type == 'M')
+            return values.isEmpty();
+        return true;
+    }
+
+    public static JSONObject parseString(String string) {
+        JSONObject currentContainer = new JSONObject();
+        Stack<JSONObject> containerStack = new Stack<JSONObject>();
+        containerStack.push(currentContainer);
+        char currentChar = ' ';
+        char type;
+        boolean expectingComma = false;
+        int index = -1;
+        while (!containerStack.empty()) {
+            currentContainer = containerStack.pop();
+            type = currentContainer.getType();
+            /// skip all whitespace
+            while ((currentChar == ' ' || currentChar == '\t' || currentChar == '\n') && index < string.length())
+            currentChar = string.charAt(++index);
+            if (type == 'U') {
+                if (currentChar == '[') { /// array
+                    type = 'A';
+                    currentContainer.type = 'A';
+                } else if (currentChar == '{') { /// map
+                    type = 'M';
+                    currentContainer.type = 'M';
+                } else if (( currentChar >= '0' && currentChar <= '9' ) || currentChar == '-') { /// integer or double                    
+                    int number = 0;
+                    int decimals = -1;
+                    if(currentChar == '-'){
+                        if(index == string.length() - 1){
+                            throw new JSONException(index, string);
+                        }
+                        number = -(string.charAt(++index) - '0');
+                    }else{
+                        number = currentChar - '0';
+                    }
+                    while(index < string.length() - 1){
+                        currentChar = string.charAt(++index);
+                        if(currentChar >= '0' && currentChar <='9'){
+                            int dig = currentChar - '0';
+                            if(number < 0)dig = -dig;
+                            number = number*10 + dig;
+                        }else
+                            break;
+                    }
+                    if(currentChar == '.'){
+                        decimals = 0;
+                        while(index < string.length() - 1){
+                            currentChar = string.charAt(++index);
+                            if(currentChar >= '0' && currentChar <='9'){
+                                decimals = decimals*10 + currentChar - '0';
+                            }else{
+                                break;
+                            }
+                        }
+                    }
+                    if(decimals == -1){
+                        currentContainer.setValue(number);
+                    }else{
+                        double d = number;
+                        if(d < 0){
+                            decimals = - decimals;
+                        }
+                        d += decimals * ( Math.pow(10, -(int)(Math.log10(decimals) + 1)));
+                        currentContainer.setValue(d);
+                    }
+                    index--;
+                } else if (currentChar == '"') { /// string
+                    type = 'S';
+                    int stringEnd = string.indexOf('\"', index + 1);
+                    if(stringEnd == -1){
+                        throw new JSONException("Error: String block started but never finished at " + Integer.toString(index));
+                    }
+                    currentContainer.setValue(string.substring(index+1, stringEnd));
+                    index = stringEnd;
+                } else if (currentChar == 'f' || currentChar == 't') { /// boolean
+                    type = 'B';
+                    if (index + 4 > string.length()) {
+                        throw new JSONException(index, string);
+                    }
+                    String value = string.substring(index, index + 4);
+                    if(value.equals("true")){
+                        currentContainer.setValue(true);
+                        index += 4;
+                    }else if(value.equals("fals")){
+                        index += 4;
+                        if(index >= string.length() || string.charAt(index) != 'e')
+                            throw new JSONException(index, string);
+                        currentContainer.setValue(false);
+                        index++;                            
+                    }else{
+                        throw new JSONException(index, string);
+                    }
+                } else if (currentChar == 'n') { /// nulll
+                    type = 'N';
+                    if (index + 4 < string.length() || !string.substring(index, index + 4).equals("null")) {
+                        throw new JSONException(index, string);
+                    }
+                    currentContainer.setNull();
+                } else {
+                    throw new JSONException(index, string);
+                }
+                if (++index < string.length()) {
+                    currentChar = string.charAt(index);
+                    /// skip all following whitespace
+                    while ((currentChar == ' ' || currentChar == '\t' || currentChar == '\n')
+                            && index < string.length())
+                        currentChar = string.charAt(++index);
+                } else {
+
+                }
+            }
+            type = currentContainer.getType();
+            if (type == 'M') {
+                if(currentChar == '"'){ /// new entry
+                    int nameEnd = string.indexOf('\"', index+1);
+                    String name = string.substring(index+1, nameEnd);
+                    JSONObject obj = new JSONObject();
+                    currentContainer.add(name, obj);
+                    containerStack.push(currentContainer);
+                    containerStack.push(obj);
+                    index = nameEnd + 1;
+                    currentChar = string.charAt(index);
+                    while ((currentChar == ' ' || currentChar == '\t' || currentChar == '\n') && index < string.length())
+                        currentChar = string.charAt(++index);
+                    if(currentChar != ':')
+                        throw new JSONException("Error: expected ':' but found '" + currentChar + "' at " + Integer.toString(index) + " while parsing string:" + string);
+                    currentChar = string.charAt(++index);
+                    while ((currentChar == ' ' || currentChar == '\t' || currentChar == '\n') && index < string.length())
+                        currentChar = string.charAt(++index);
+                }else if(currentChar == ','){ 
+                    currentChar = string.charAt(++index);
+                    containerStack.push(currentContainer);                    
+                }else if(currentChar == '}'){ /// end of map
+                    if(!containerStack.empty()){
+                        JSONObject parentContainer = containerStack.peek();
+                        if(parentContainer.getType() == 'M' || parentContainer.getType() == 'A'){
+                            parentContainer.add(currentContainer);
+                        }else{
+                            throw new JSONException("Error: invalid JSONObject type '" + parentContainer.getType() + "'...");
+                        }
+                    }
+                    index++;
+                    if(index < string.length())currentChar = string.charAt(index);
+                }else{
+                    containerStack.push(currentContainer);                    
+                    containerStack.push(new JSONObject());
+                }
+
+            }else if (type == 'A') {
+                if(currentChar == ','){
+                    currentChar = string.charAt(++index);
+                    containerStack.push(currentContainer);                    
+                    containerStack.push(new JSONObject());
+                }else if(currentChar == ']'){ /// end of array
+                    if(!containerStack.empty()){
+                        JSONObject parentContainer = containerStack.peek();
+                        if(parentContainer.getType() == 'M' || parentContainer.getType() == 'A'){
+                            parentContainer.add(currentContainer);
+                        }else{
+                            throw new JSONException("Error: invalid JSONObject type '" + parentContainer.getType() + "'...");
+                        }
+                    }
+                    index++;
+                    if(index < string.length())currentChar = string.charAt(index);
+                }else{
+                    containerStack.push(currentContainer);                    
+                    containerStack.push(new JSONObject());
+                }
+            }else if(type == 'I' || type == 'D' || type == 'S' || type == 'B' || type == 'N'){
+                if(!containerStack.empty()){
+                    JSONObject parentContainer = containerStack.peek();
+                    if(parentContainer.getType() == 'M' || parentContainer.getType() == 'A'){
+                        parentContainer.add(currentContainer);
+                    }else{
+                        throw new JSONException("Error: invalid JSONObject type '" + parentContainer.getType() + "'...");
+                    }
+                }
+            }
+        }
+        if(index != string.length()){
+            throw new JSONException(index, string);
+        }
+        return currentContainer;
     }
 
     public Iterator<String> keys() {
@@ -127,14 +316,17 @@ public class JSONObject {
     }
 
     public void add(JSONObject value) {
-        if (type != 'A' && type != 'N') /// it is not an array or NULL
+        if (type != 'A' && type != 'M' && type != 'U') /// it is not an array or NULL
             throw new JSONException("Error: JSONObject is not of type array");
 
-        if (type == 'N') {
+        if (type == 'U')
             type = 'A';
+        if(values == null)
             values = new ArrayList<JSONObject>();
-        }
-        values.add(value);
+        if(type == 'A')
+            values.add(value);
+        if(type == 'M')
+            values.set(values.size()-1, value);
     }
 
     public void add(int i) {
@@ -162,14 +354,15 @@ public class JSONObject {
     }
 
     public void add(String key, JSONObject value) {
-        if (type != 'M' && type != 'N')
+        if (type != 'M' && type != 'U')
             throw new JSONException("Error: JSONObject is not of type map...");
 
-        if (type == 'N') {
-            keys = new ArrayList<String>();
-            values = new ArrayList<JSONObject>();
+        if (type == 'U')
             type = 'M';
-        }
+        if(keys == null)
+            keys = new ArrayList<String>();
+        if(values == null)
+            values = new ArrayList<JSONObject>();
         keys.add(key);
         values.add(value);
     }
@@ -190,14 +383,8 @@ public class JSONObject {
     }
 
     public static void main(String[] args) {
-        JSONObject obj = new JSONObject();
-
-        obj.add(new JSONObject("test"));
-        obj.add(new JSONObject(false));
-
-        System.out.println(obj.toString());
-        System.out.println(obj.get(0));
-        System.out.println(obj.get(1));
-        System.out.println(obj.get("test"));
+        String str = new String("[{\"a\":[1, 2], \"abc\":12}, \"test\"]");
+        JSONObject obj = JSONObject.parseString(str);
+        System.out.println(obj);
     }
 }
